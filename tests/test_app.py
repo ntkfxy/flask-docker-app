8c7812f766b772293e8958882d26f26f31d952af
ntkfxy <natthaan57101@gmail.com>
2026-03-25 15:04:18 +0700

    fix: update test case to match actual output

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,7 +20,7 @@ def test_hello_endpoint(client):
     # ตรวจสอบ response data
     json_data = response.get_json()
     assert json_data is not None
-    assert json_data['message'] == 'Hello Nabnoey'
+    assert json_data['message'] == 'Hello Natthakan Jamratphum!'
 
 
 def test_hello_endpoint_method_not_allowed(client):
